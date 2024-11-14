31a4c06b353c206e7b9db93509c822702a992a9a
KurtDV21 <vergarakurt.bsit@gmail.com>
2024-11-14 17:56:51 +0800

    homepage and userhomepage ni aaronn

--- resource/view/home.php
+++ resource/view/home.php
@@ -32,150 +32,150 @@ if (isset($_SESSION['user_id'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="/css/user.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/styletab.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Aoboshi+One&display=swap">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
 </head>
 <body>
-  <!-- header nav-bar -->
-  <header>
-  <nav class="navbar">
+<header>
+    <nav class="navbar">
     <img src="/image/logo1.png" alt="logo" class="logo">
 
-    <div class="nav-container">
-      <div class="hamburger" onclick="toggleMenu(this)">
-          <div class="bar"></div>
-          <div class="bar"></div>
-          <div class="bar"></div>
-      </div>
+    
+        <div class="hamburger" onclick="toggleMenu()">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
 
-      <ul class="nav-link">
+    <ul class="nav-link">
         <li><a href="#home">HOME</a></li>
         <li><a href="#ourcat">OUR CATS</a></li>
         <li><a href="">ABOUT</a></li>
         <li><a href="">FAQs</a></li>
-        <li><button class="login-btn" onclick="location.href='/loginto'">Login</button></li>     </ul>
-    </div>
-  </nav>
-</header>
+        <li><button class="login-btn" onclick="location.href='/loginto'">Login</button></li>     
+    </ul>
+    </nav>
+  </header>
 
-  <!-- body -->
-  <!-- HOME SECTION -->
-  <section id="home">
-    <div class="main">
-      <h1 class="text1">Find Your Purr-fect<br> Companion: Adopt<br> a Cat Today!</h1>
-    </div>
-    <div class="adopt-btn-container">
-    <button class="adopt-btn button" onclick="scrollToCats()">ADOPT NOW!</button>
+
+  <section id="main" class="main">
+    <div class="main-container">
+    <h1 class="text1">Find Your Purr-fect<br> Companion: Adopt<br> a Cat Today!</h1>
+    <button class="adopt-btn">ADOPT NOW!</button>
     </div>
   </section>
 
-  <!-- DROPDOWN BUTTONS -->
-  <section id="dropbtn">
-    <div class="dropdowns">
-      <!-- First Dropdown -->
-      <div class="dropdown">
-        <button class="dropbtn">
-          <span class="button-text"><b> COLOR </b></span> 
-          <span class="arrow-down"><ion-icon name="chevron-down"></ion-icon></span>
-          <span class="paint"><ion-icon name="color-palette-outline"></ion-icon></span>
-        </button>
-        <div class="dropdown-content">
-          <a href="#">Category 1</a>
-          <a href="#">Category 2</a>
-          <a href="#">Category 3</a>
-        </div>
-      </div>
 
-      <!-- Second Dropdown -->
-      <div class="dropdown">
-        <button class="dropbtn">
-          <span class="button-text"><b>GENDER</b></span>
-          <span class="arrow-down-1"><ion-icon name="chevron-down"></ion-icon></span>
-          <span class="gender"><ion-icon name="male-female-outline"></ion-icon></span>
-        </button>
-        <div class="dropdown-content">
-          <a href="#">Item 1</a>
-          <a href="#">Item 2</a>
-          <a href="#">Item 3</a>
+
+
+  <section id="ourcats" class="ourcats">
+  <div class="container">
+    <div class="buttons-container">
+        <div class="dropdown">
+            <button class="dropbtn">
+                <span class="button-text"><b>COLOR</b></span>
+                <span class="arrow-down"><ion-icon name="chevron-down"></ion-icon></span>
+                <span class="paint"><ion-icon name="color-palette-outline"></ion-icon></span>
+            </button>
+            <div class="dropdown-content">
+                <a href="#">Category 1</a>
+                <a href="#">Category 2</a>
+                <a href="#">Category 3</a>
+            </div>
         </div>
-      </div>
+
+        <div class="dropdown">
+            <button class="dropbtn">
+                <span class="button-text"><b>GENDER</b></span>
+                <span class="arrow-down-1"><ion-icon name="chevron-down"></ion-icon></span>
+                <span class="gender"><ion-icon name="male-female-outline"></ion-icon></span>
+            </button>
+            <div class="dropdown-content">
+                <a href="#">Item 1</a>
+                <a href="#">Item 2</a>
+                <a href="#">Item 3</a>
+            </div>
+        </div>
+
+        <div class="dropdown"> 
+            <li>
+           <button class="addbtn" onclick="location.href='/add-post?user_id=<?php echo htmlspecialchars($userId); ?>'"> Add Post </button>
+            </li>
+        </div> 
+ 
     </div>
-       
-    <div class="add-container">
-      <li>
-        <button class="addbtn" onclick="location.href='/loginto'"> Add Post </button>
-      </li>
-    </div>    
-  </section>
-
-  <!-- OUR CATS SECTION -->
-  <h1 class="avail"><b>AVAILABLE CATS</b></h1>
-  <section id="ourcat">
+       <h1 class="avail"><b>AVAILABLE CATS</b></h1>
+  
+       <div class="cat-container">
   <?php foreach ($approvedPosts as $post): ?>
-    <div class="cat-container" >
+    <div class="cat-wrapper">
       <?php if (isset($_SESSION['user_id'])): ?>
         <a href="/cat-details?post_id=<?php echo urlencode($post['id']); ?>">
       <?php else: ?>
         <a href="/loginto">
       <?php endif; ?>
-        <div class="cat-wrapper">
-          <div class="cat-card" id="ourcats">
-            <img src="<?php echo htmlspecialchars($post['picture']); ?>" alt="Cat Image">
+          <div class="cat-card">
+            <img src="<?php echo htmlspecialchars($post['picture']); ?>" alt="Cat Image" class="cat-image">
             <h2><?php echo htmlspecialchars($post['cat_name']); ?></h2>
             <p>Age: <?php echo htmlspecialchars($post['age']); ?> years</p>
             <p>Location: <?php echo htmlspecialchars($post['location']); ?></p>
           </div>
-          <button class="adopt-btn1">Adopt Me</button>
-        </div>
-      </a>
+          <button class="btn-adopt">Adopt Me</button>
+        </a>
     </div>
   <?php endforeach; ?>
+</div>
+
+  </div>
 </section>
 
 
-  <footer class="footer">
+<section id="about" class="about">
     <div class="footer-container">
-      <!-- About Section -->
-      <div class="footer-about">
+        <div class="about-company">
+
+        <div class="info-item">
+        <img src="/image/place.png" alt="" class="place-icon">
+        <p><a href="">9A Masambong St. Bahay Toro, Quezon City</a></p>
+    </div>
+    <div class="info-item">
+        <img src="/image/phone.png" alt="" class="phone-icon">
+        <p><a href="">09123456789</a></p>
+    </div>
+    <div class="info-item">
+        <img src="/image/email.png" alt="" class="email-icon">
+        <p><a href="">catfreeadopt@email.com</a></p>
+    </div>
+        </div>
+
+
+        <div class="details">
         <h3>ABOUT COMPANY</h3>
-        <div class="para">
-          <p>Lorem ipsum dolor sit amet. Ex officiis molestias et sapiente<br> doloremque et dolores doloribus est animi maiores. Ut fugiat <br> molestiae nam quia earum qui aliquid aliquid ab corrupti officiis. Et<br> temporibus quia 33 incidunt adipisci ea deleniti vero 33<br> reprehenderit repellat.</p>
+            <p>Lorem ipsum dolor sit amet. Ex officiis molestias et sapiente<br> doloremque et dolores doloribus est animi maiores. Ut fugiat <br> molestiae nam quia earum qui aliquid aliquid ab corrupti officiis. Et<br> temporibus quia 33 incidunt adipisci ea deleniti vero 33<br> reprehenderit repellat.</p>
+            
+            
+            <a href="https://www.facebook.com/groups/1591906714301364" target="_blank">
+                <img src="/image/facebook.png" alt="Facebook" class="fb-icon">
+            </a>
+            
+            <a href="https://www.messenger.com" target="_blank">
+                <img src="/image/messenger.png" alt="Messenger" class="mess-icon">
+            </a>
+     
         </div>
-      </div>
-      <!-- Social Media Section -->
-      <div class="fb">
-        <img src="/image/facebook.png" alt="" class="fb-icon">
-      </div>
-      <div class="mess">
-        <img src="/image/messenger.png" alt="" class="mess-icon">
-      </div>
     </div>
 
-    <div class="details-container">
-      <div class="details">
-        <div class="place">
-          <img src="/image/place.png" alt="" class="place-icon">
-          <p>ASDWQDASDASDASDASDASDASDASD</p>
-        </div>
-        <div class="phone">
-          <img src="/image/phone.png" alt="" class="phone-icon">
-          <p>ASDWQDASDASDASDASDASDASDASD</p>
-        </div>
-        <div class="email">
-          <img src="/image/email.png" alt="" class="email-icon">
-          <p>ASDWQDASDASDASDASDASDASDASD</p>
-        </div>
-      </div>
-    </div>
+  </section>
+  
 
-    <!-- Left Bottom Text -->
-    <div class="footer-left-text">
-      <p>Cat Free Adoption & Rescue Philippines</p>
-    </div>
+  <footer class = "footer">
+    Cats Free Adoption & Rescue Philippines
   </footer>
+  
 
   <script src="/js/package.js"></script>
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
